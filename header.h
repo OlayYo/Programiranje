6fc527f9a478881a64d5ca82e2d0ff393cb95fe8
OlayYo <olay.younis18@gmail.com>
2025-05-26 17:40:58 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>      // 7. Organizacija izvornog koda
+#include <stdlib.h>     // 7. Organizacija izvornog koda
+#include <string.h>     // 7. Organizacija izvornog koda
+
+// 8. extern deklaracija
+extern int globalBrojKvarova;
+
+// 9. makro konstante za velicine polja
+#define MAX_REG 12
+#define MAX_OPIS 256
+
+// 4. typedef, enum za status popravka
+typedef enum {
+    STATUS_CEKA_DIJELOVE,   // 4. enum
+    STATUS_POPRAVLJENO,     // 4. enum
+    STATUS_U_TOKU,          // 4. enum
+    STATUS_ODBIJENO         // 4. enum
+} StatusPopravka;
+
+// 13. 3. Složeni tipovi, 4. typedef, 16. Dinamička memorija (povezani popis)
+typedef struct kvar_popravak {
+    char registracija[MAX_REG];      // 9. koristenja makroa
+    char opis[MAX_OPIS];             // 9. koristenja makroa
+    char datum[11];             // 2. char[], 15. Statički zauzeto polje
+    float cijena;               // 2. float
+    StatusPopravka status;      // 4. enum
+    struct kvar_popravak* next; // 12. Pokazivač, 16. Dinamička memorija
+} KvarPopravak;                 // 3. Složeni tip, 4. typedef
+
+// 5. Imenovanje identifikatora (camelCase, snake_case dosljedno)
+// 7. Organizacija izvornog koda (deklaracije u headeru)
+void dodajKvarPopravak(KvarPopravak** glava);
+void ispisiSveKvarove(const KvarPopravak* glava);
+void azurirajKvarPopravak(KvarPopravak* glava);
+void obrisiKvarPopravak(KvarPopravak** glava);
+void sortirajKvarove(KvarPopravak** glava);
+void pretraziKvarPopravak(const KvarPopravak* glava);
+void spremiKvarove(const KvarPopravak* glava, const char* filename);
+void ucitajKvarove(KvarPopravak** glava, const char* filename);
+void oslobodiMemoriju(KvarPopravak** glava);
+const char* statusToString(StatusPopravka status);
+void sortirajCijeneQsort(const KvarPopravak* glava); // 25, 26
---
+++ main.c
@@ -0,0 +1,99 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "header.h"
+
+// 11. enum za izbornik, 5. Imenovanje identifikatora
+typedef enum {
+    DODAJ = 1,
+    ISPIS,
+	PRETRAZI,
+    AZURIRAJ,
+    OBRISI,
+    SORTIRAJ,
+	SORTIRAJ_CIJENE_QSORT, // 25, 26
+    SPREMI,
+    UCITAJ,
+	BRISI_DATOTEKU,  // 21. opcija za brisanje datoteke
+    IZLAZ = 11
+ 
+} IzbornikOpcije;
+
+int main(void) {
+    KvarPopravak* glava = NULL; // 12. Pokazivač na početak liste
+    int odabir = 0;             // 2. int
+    const char* filename = "kvarovi.bin"; // 2. char*, 15. Statički zauzeto polje
+    do {
+        // 10. Izbornik
+        printf("\n--- Evidencija kvarova i popravaka vozila ---\n");
+        printf("1 -> Dodaj novi kvar/popravak\n");
+        printf("2 -> Ispisi sve kvarove\n");
+		printf("3 -> Pretrazi po registraciji\n");
+        printf("4 -> Azuriraj kvar\n");
+        printf("5 -> Obrisi kvar\n");
+        printf("6 -> Sortiraj po datumu\n");
+		printf("7 -> Sortiraj sve cijene\n"); // 25, 26
+        printf("8 -> Spremi u datoteku\n");
+        printf("9 -> Ucitaj iz datoteke\n");
+		printf("10 -> Obrisi datoteku\n");
+        printf("11 -> Izlaz\n");
+       
+        printf("Odabir: ");
+        scanf("%d", &odabir);
+
+        switch (odabir) { // 10. Izbornik, 11. enum
+
+        case DODAJ:
+            dodajKvarPopravak(&glava);   // 1. Create
+            break;
+
+        case ISPIS:
+            ispisiSveKvarove(glava);     // 1. Read
+            break;
+
+        case AZURIRAJ:
+            azurirajKvarPopravak(glava); // 1. Update
+            break;
+
+        case OBRISI:
+            obrisiKvarPopravak(&glava);  // 1. Delete
+            break;
+
+        case SORTIRAJ:
+            sortirajKvarove(&glava);     // 23. Sortiranje
+            break;
+
+        case PRETRAZI:
+            pretraziKvarPopravak(glava); // 24. Pretraživanje
+            break;
+
+        case SPREMI:
+            spremiKvarove(glava, filename); //19. Datoteke
+            break;
+
+        case UCITAJ:
+            ucitajKvarove(&glava, filename); // 19. Datoteke
+            break;
+
+		case BRISI_DATOTEKU:
+			if (remove(filename) == 0) {
+				printf("Datoteka obrisana.\n");
+			}
+			else {
+				perror("Greska pri brisanju datoteke.");
+			}
+			break;
+
+        case SORTIRAJ_CIJENE_QSORT:
+            sortirajCijeneQsort(glava); // 25, 26
+            break;
+
+        case IZLAZ:
+            oslobodiMemoriju(&glava);    // 18. Oslobađanje memorije
+            printf("Izlazak iz programa.\n");
+            break;
+        default:
+            printf("Pogresan unos!\n");
+        }
+    } while (odabir != IZLAZ);
+
+    return 0;
+}
